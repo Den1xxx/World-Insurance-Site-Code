8568901d6f54366bc442133f67be5e4f67fcc266
cubanresourceful <cubanresourceful@users.noreply.github.com>
2015-01-26 19:24:47 -0500

    Added addUser.php, not sure why VS didn't add the file by default...

---
+++ World Insurance/includes/addUser.php
@@ -0,0 +1,47 @@
+<?php
+
+    // Determine site content root
+    define('__ROOT__', dirname(dirname(__FILE__)));
+    
+    require_once( __ROOT__ . "/config.php" );
+    require_once( __ROOT__ . "/includes/database.php" );
+    require_once( __ROOT__ . "/includes/createHash.php" );
+    
+    $dbObject = new Database;
+    $db = $dbObject->createDatabaseConnection();
+
+    // Checks the database connection
+    if ($db->connect_errno) {
+        
+        error_log( "Connection failed: " . $db->connect_error );
+        return FALSE;
+        
+    }
+    
+    $newUserEmail           = $_POST[ 'inputUserEmail' ];
+	$newUserPass            = $_POST[ 'inputUserPass' ];
+    $newUserAccountNumber   = $_POST[ 'inputUserAccountNumber' ];
+	$newIsAdmin             = $_POST[ 'inputIsAdmin' ];
+    $hashUserPass           = create_hash("$newUserPass");
+    
+    $preppedNewUserEmail            = $db->real_escape_string($newUserEmail);
+    $preppedNewUserPass             = $db->real_escape_string($hashUserPass);
+    $preppedNewUserAccountNumber    = $db->real_escape_string($newUserAccountNumber);
+    $preppedNewIsAdmin              = $db->real_escape_string($newIsAdmin);
+    
+    $SQLQuery = "INSERT INTO `cm`.`CM_Users` (`userRecordID`, `isAdmin`, `userEmail`, `userPassword`, `accountNumber`) VALUES (NULL, '$preppedNewIsAdmin', '$preppedNewUserEmail', '$preppedNewUserPass', '$preppedNewUserAccountNumber');";
+    
+    //$SQLQuery = $db->real_escape_string($SQLQuery);
+    
+    if ( $db->query($SQLQuery) === FALSE ) {
+        
+        error_log( "Error creating table: " . $db->error );
+        return FALSE;
+        
+    }
+
+    $db->close();
+    
+    return TRUE;
+
+?>
