c0c16fd8f20501bf042edf3fc40001eaed25d176
cubanresourceful <cubanresourceful@users.noreply.github.com>
2015-01-30 21:40:30 -0500

    Small fix to policies

--- World Insurance/account/policies.php
+++ World Insurance/account/policies.php
@@ -10,8 +10,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <?php echo "<title>_SESSION['firstName']'s Policies</title>"; ?>
-        <title>My Policy</title>
+        <?php echo "<title>$_SESSION['firstName']'s Policies</title>"; ?>
 
         <!-- Bootstrap CSS 3.3.2 -->
         <link href="css/bootstrap.min.css" rel="stylesheet" />
