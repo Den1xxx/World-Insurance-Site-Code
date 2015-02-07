1d63482eeb61896e21333c0f9d8b61fe95324f3e
cubanresourceful <cubanresourceful@users.noreply.github.com>
2015-02-07 21:35:01 +0000

    Fixed tabs, made SQL queries db name agnostic

--- includes/loginUser.php
+++ includes/loginUser.php
@@ -1,120 +1,124 @@
 <?php
 
-    // Starts a new session
-    ob_start();
-    session_start();
+	// Starts a new session
+	ob_start();
+	session_start();
 
-    // Determine site content root
-    define('__ROOT__', dirname(dirname(__FILE__)));
-    
-    require_once( __ROOT__ . "/config.php" );
-    require_once( __ROOT__ . "/includes/database.php" );
-    require_once( __ROOT__ . "/includes/createHash.php" );
-    
-    // Setups up return array
-    $ret = array(
+	// Determine site content root
+	define('__LOUSROOT__', dirname(dirname(__FILE__)));
+
+	require_once( __LOUSROOT__ . "/config.php" );
+	require_once( __LOUSROOT__ . "/includes/database.php" );
+	require_once( __LOUSROOT__ . "/includes/createHash.php" );
+
+	// Setups up return array
+	$ret = array(
 		'returnStatus' => "",
 		'errorLog' => ""
 	);
-    
-    $dbObject = new Database;
-    $db = $dbObject->createDatabaseConnection();
-    
-    // Checks the database connection
-    if ($db->connect_errno) {
-        
-        error_log( "Connection failed: " . $db->connect_error );
-        
-        $ret["returnStatus"] = "Fail";
-        $ret["errorLog"] = "Connection failed: " . $db->connect_error;
-        
-        echo json_encode($ret);
-        
-    }
-    
-    $loginUserEmail           = $_GET[ 'inputLoginEmail' ];
-	$loginUserPass            = $_GET[ 'inputLoginUserPass' ];
-    
-    $preppedLoginUserEmail  = $db->real_escape_string($loginUserEmail);
-    $preppedLoginUserPass   = $db->real_escape_string($loginUserPass);
-    
-    $SQLQuery = "SELECT * FROM `cm`.`CM_Users` WHERE `userEmail` = '$preppedLoginUserEmail';";
-    
-    $result = $db->query($SQLQuery);
-    
-    if ( $result === FALSE ) {
-        
-        error_log( "Error finding user record: " . $db->error );
-        
-        $ret["returnStatus"] = "Fail User";
-        $ret["errorLog"] = "Error finding user record: " . $db->error;
-        
-        echo json_encode($ret);
-        
-    }
-    else {
-    
-        // Fetch returned row
-        $row = $result->fetch_row();
-        
-        // Close the returned result
-        $result->close();
-        
-        if( $row[2] == $preppedLoginUserEmail && validate_password($preppedLoginUserPass, $row[3])  ) {
-            
-            $_SESSION["userEmail"] = "$row[2]";
-            $_SESSION["isAdmin"] = "$row[1]";
-            $_SESSION["accountNumber"] = "$row[4]";
-            
-            $accountNumber = "$row[4]";
-            
-            $SQLQuery = "SELECT * FROM `cm`.`CM_Customers` WHERE `accountNumber` = '$accountNumber';";
-            
-            $result = $db->query($SQLQuery);
-            
-            if ( $result === FALSE ) {
-                
-                error_log( "Error finding customer record: " . $db->error );
-                
-                $ret["returnStatus"] = "Fail Customer";
-                $ret["errorLog"] = "Error finding customer record: " . $db->error;
-                
-                echo json_encode($ret);
-                
-            }
-            else {
-                
-                // Fetch returned row
-                $row = $result->fetch_row();
-                
-                // Close the returned result
-                $result->close();
-                
-                $_SESSION["firstName"] = "$row[2]";
-                $_SESSION["lastName"] = "$row[3]";
-                $_SESSION["zip"] = "$row[4]";
-                $_SESSION["customerPolicyNumbers"] = "$row[5]";
-                $_SESSION["customerPolicyPDFs"] = "$row[6]";
-                
-                $ret["returnStatus"] = "Success";
-                
-                echo json_encode($ret);
-                
-            }
-            
-        }
-        else {
-            
-            $ret["returnStatus"] = "Fail User";
-            $ret["errorLog"] = "Bad login";
-            
-            echo json_encode($ret);
-            
-        }
-    
-    }
-    
-    // Close the database connection
-    $db->close();
+
+	$dbObject = new Database;
+	$db       = $dbObject->createDatabaseConnection();
+
+	// Checks the database connection
+	if ($db->connect_errno) {
+
+		error_log( "Connection failed: " . $db->connect_error );
+
+		$ret["returnStatus"] = "Fail";
+		$ret["errorLog"]     = "Connection failed: " . $db->connect_error;
+
+		echo json_encode($ret);
+
+	}
+
+	$loginUserEmail = $_GET[ 'inputLoginEmail' ];
+	$loginUserPass  = $_GET[ 'inputLoginUserPass' ];
+
+	$preppedLoginUserEmail = $db->real_escape_string($loginUserEmail);
+	$preppedLoginUserPass  = $db->real_escape_string($loginUserPass);
+
+	$SQLQuery = "SELECT * FROM `" . DB_NAME . "`.`CM_Users`" .
+		"WHERE `userEmail` = '$preppedLoginUserEmail';";
+
+	$result = $db->query($SQLQuery);
+
+	if ( $result === FALSE ) {
+
+		error_log( "Error finding user record: " . $db->error );
+
+		$ret["returnStatus"] = "Fail User";
+		$ret["errorLog"]     = "Error finding user record: " . $db->error;
+
+		echo json_encode($ret);
+
+	}
+	else {
+
+		// Fetch returned row
+		$row = $result->fetch_row();
+
+		// Close the returned result
+		$result->close();
+
+		if( $row[2] == $preppedLoginUserEmail &&
+			validate_password($preppedLoginUserPass, $row[3])  ) {
+
+			$_SESSION["userEmail"]     = "$row[2]";
+			$_SESSION["isAdmin"]       = "$row[1]";
+			$_SESSION["accountNumber"] = "$row[4]";
+
+			$accountNumber = "$row[4]";
+
+			$SQLQuery = "SELECT * FROM `" . DB_NAME . "`.`CM_Customers`" .
+				"WHERE `accountNumber` = '$accountNumber';";
+
+			$result = $db->query($SQLQuery);
+
+			if ( $result === FALSE ) {
+
+				error_log( "Error finding customer record: " . $db->error );
+
+				$ret["returnStatus"] = "Fail Customer";
+				$ret["errorLog"]     = "Error finding customer record: " .
+					$db->error;
+
+				echo json_encode($ret);
+
+			}
+			else {
+
+				// Fetch returned row
+				$row = $result->fetch_row();
+
+				// Close the returned result
+				$result->close();
+
+				$_SESSION["firstName"]             = "$row[2]";
+				$_SESSION["lastName"]              = "$row[3]";
+				$_SESSION["zip"]                   = "$row[4]";
+				$_SESSION["customerPolicyNumbers"] = "$row[5]";
+				$_SESSION["customerPolicyPDFs"]    = "$row[6]";
+
+				$ret["returnStatus"] = "Success";
+
+				echo json_encode($ret);
+
+			}
+
+		}
+		else {
+
+			$ret["returnStatus"] = "Fail User";
+			$ret["errorLog"]     = "Bad login";
+
+			echo json_encode($ret);
+
+		}
+
+	}
+
+	// Close the database connection
+	$db->close();
 
 ?>
