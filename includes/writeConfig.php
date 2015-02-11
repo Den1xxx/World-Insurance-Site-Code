f840d6fd19ad1ac9b403caf6440ef7bb46cc9b5a
cubanresourceful <cubanresourceful@users.noreply.github.com>
2015-02-10 21:04:31 -0500

    Preliminary work on generalizing the code more, exposing more things as constants, to increase maintainability of the code: change one, change everywhere

--- includes/writeConfig.php
+++ includes/writeConfig.php
@@ -1,54 +1,76 @@
 <?php
-   
-    // Determine site content root
-    define('__ROOT__', dirname(dirname(__FILE__)));
-   
-    $newDatabaseName     = $_POST[ 'inputDatabaseName' ];
-	$newDatabaseUserName = $_POST[ 'inputDatabaseUserName' ];
-	$newDatabaseUserPass = $_POST[ 'inputDatabaseUserPass' ];
-	$newDatabaseHostname = $_POST[ 'inputDatabaseHostname' ];
-   
-   $configDatabaseName     = "'" . "$newDatabaseName" . "'";
-   $configDatabaseUserName = "'" . "$newDatabaseUserName" . "'";
-   $configDatabaseUserPass = "'" . "$newDatabaseUserPass" . "'";
-   $configDatabaseHostname = "'" . "$newDatabaseHostname" . "'";
-   
-   $ifp = fopen( __ROOT__ . "/config.php", "w" ) or die( "Unable to open file!" );
-   
-   $txt = "<?php
-   
-   // Website top level directory path
-   define('TOP_PATH', '/');
-   
-   ";
-   fwrite($ifp, $txt);
-   
-   $txt = "// ** MySQL settings - You can get this info from your web host ** //
-   /** The name of the database for WordPress */
-   define('DB_NAME', $configDatabaseName);
-   
-   /** MySQL database username */
-   define('DB_USER', $configDatabaseUserName);
-   
-   /** MySQL database password */
-   define('DB_PASSWORD', $configDatabaseUserPass);
-   
-   /** MySQL hostname */
-   define('DB_HOST', $configDatabaseHostname);
-   
-   /** Database Charset to use in creating database tables. */
-   define('DB_CHARSET', 'utf8');
-   
-   /** The Database Collate type. Don't change this if in doubt. */
-   define('DB_COLLATE', '');
-   
+
+	// Determine site content root
+	$webroot = dirname(dirname(__FILE__));
+
+	$configDatabaseName     = $_POST[ 'inputDatabaseName' ];
+	$configDatabaseUserName = $_POST[ 'inputDatabaseUserName' ];
+	$configDatabaseUserPass = $_POST[ 'inputDatabaseUserPass' ];
+	$configDatabaseHostname = $_POST[ 'inputDatabaseHostname' ];
+
+	$configSiteName = "World Insurance";
+
+	$configTblCustomer = "tblCustomer";
+	$configTblUser = "tblUser";
+	$configTblPolicy = "tblPolicy";
+
+	$ifp = fopen( $webroot . "/config.php", "w" ) or die( "Unable to open file!" );
+
+	$txt = "<?php
+
+	// Website top level directory path
+	define('TOP_PATH', '$webroot');
+
+	";
+	fwrite($ifp, $txt);
+
+	$txt =
+	"  /*
+	* MySQL settings
+	*/
+
+	// MySQL database name
+	define('DB_NAME', '$configDatabaseName');
+
+	// MySQL database username
+	define('DB_USER', '$configDatabaseUserName');
+
+	// MySQL database password
+	define('DB_PASSWORD', '$configDatabaseUserPass');
+
+	// MySQL hostname
+	define('DB_HOST', '$configDatabaseHostname');
+
+	// Database Charset to use in creating database tables.
+	define('DB_CHARSET', 'utf8');
+
+	// The Database Collate type.
+	define('DB_COLLATE', '');
+
+	/*
+	* App Config
+	*/
+
+	// Website Name
+	define('SITE_NAME', '$configSiteName');
+
+	// Customers table name
+	define('TBL_CUSTOMER', '$configTblCustomer');
+
+	// Users table name
+	define('TBL_USER', '$configTblUser');
+
+	// Policies table name
+	define('TBL_POLICY', '$configTblPolicy');
+
 ?>
-   ";
-   fwrite($ifp, $txt);
-   fclose($ifp);
-   
-   $configExists = file_exists( __ROOT__ . "/config.php" );
-   
-   return $configExists;
-   
+";
+
+	fwrite($ifp, $txt);
+	fclose($ifp);
+
+	$configExists = file_exists( __ROOT__ . "/config.php" );
+
+	return $configExists;
+
 ?>
